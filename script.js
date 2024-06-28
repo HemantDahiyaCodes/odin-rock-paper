3b0688436bf2098d822ea3a133adbcd19b7a97e2
HemantDahiyaCodes <HemnatDahiyaCodes@gmail.com>
2024-06-28 08:52:30 +0530

    .

--- script.js
+++ script.js
@@ -14,65 +14,69 @@
 // Paragraph
 const para = document.querySelector("p");
 // Computer choice
+function computerChoice() {
 let computerInput = Math.random();
-let computerChoice;
+let computer_Choice;
 
 if (computerInput >= 0.33 && computerInput < 0.34) {
-  computerChoice = "rock";
+  computer_Choice = "rock";
 
 } else if (computerInput >= 0.35 && computerInput < 0.65) {
-  computerChoice = "paper";
+  computer_Choice = "paper";
 
 } else if (computerInput >= 0.66 && computerInput < 0.99) {
-  computerChoice = "scissors";
+  computer_Choice = "scissors";
 }
-
+ return computer_Choice;
+}
 let userChoice;
 
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", () => {
   userChoice = "rock";
 //   console.log(userChoice);
-  playRound(userChoice, computerChoice);
+  playRound(userChoice, computerChoice());
 });
 
 const paper = document.querySelector("#paper");
 paper.addEventListener("click", () => {
   userChoice = "paper";
 //   console.log(userChoice);
-  playRound(userChoice, computerChoice);
+  playRound(userChoice, computerChoice());
 });
 
 const scissors = document.querySelector("#Scissors");
 scissors.addEventListener("click", () => {
   userChoice = "scissors";
 //   console.log(userChoice);
-  playRound(userChoice, computerChoice);
+  playRound(userChoice, computerChoice());
 });
 
 
 // PlayRound Function.
 function playRound(userSelection, computerSelection) {
     userSelection = userChoice;
-    computerSelection = computerChoice;
+    computerSelection = computerChoice();
 
     // Comparing both inputs and declaring the winner;
     
 // if both of the inputs are same.
     if (userSelection == computerSelection) {
         // document.write("It's a tie");
-        para.textContent = "It's a tie";
+        para.textContent = `You chose: ${userSelection}`;
+        para.textContent = `Computer chose: ${computerSelection}`;
+        para.textContent = `You chose ${userSelection}. Computer chose ${computerSelection}. It's a tie`;
     }
 
 // For the inputs where human will win
     else if (userSelection == "rock" && computerSelection =="scissors" || userSelection == "paper" && computerSelection == "rock" || userSelection == "scissors" && computerSelection == "paper") {
         // document.write(`User won`);
-        para.textContent = "User won";
+        para.textContent = `You chose ${userSelection}. Computer chose ${computerSelection}. You won`;
     }
 
 // For the inputs where computer will win
     else if (userSelection == "scissors" && computerSelection =="rock" || userSelection == "rock" && computerSelection == "paper" || userSelection == "paper" && computerSelection == "scissors") {
         // document.write(`Computer won`);
-        para.textContent = "Computer won";
+        para.textContent = `You chose ${userSelection}. Computer chose ${computerSelection}. Computer won`;
     }
 };
